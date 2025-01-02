f26273c818ffadcf0d9f031866af80037da44f6d
Nimer-Asaad <nimerziad42@gmail.com>
2025-01-02 17:01:46 +0200

    first Push Start Page

---
+++ StartPage.js
@@ -0,0 +1,7 @@
+// Example: Add event listener to all buttons
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+      alert('Workout Plan Selected!');
+    });
+  });
+  
